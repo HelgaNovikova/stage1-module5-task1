c3440d2bb6c3e643afee029a506c86eb8e510036
HelgaNovikova <helpotaki@rambler.ru>
2022-11-23 17:10:26 -0500

    st1m5t1

--- src/main/java/com/epam/mjc/InterfaceCreator.java
+++ src/main/java/com/epam/mjc/InterfaceCreator.java
@@ -1,32 +1,77 @@
 package com.epam.mjc;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.function.BiFunction;
-import java.util.function.Consumer;
-import java.util.function.Function;
-import java.util.function.Predicate;
-import java.util.function.Supplier;
+import java.util.function.*;
 
 public class InterfaceCreator {
 
     public Predicate<List<String>> isValuesStartWithUpperCase() {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return x -> {
+            for (String str : x) {
+                char[] array = str.toCharArray();
+                if (!Character.isUpperCase(array[0])) {
+                    return false;
+                }
+            }
+            return true;
+        };
     }
 
     public Consumer<List<Integer>> addEvenValuesAtTheEnd() {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return x -> {
+            List<Integer> res = new ArrayList<>();
+            for (int item : x) {
+                if (item % 2 == 0) {
+                    res.add(item);
+                }
+            }
+            x.addAll(res);
+        };
     }
 
     public Supplier<List<String>> filterCollection(List<String> values) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return () -> {
+            List<String> res = new ArrayList<>(values);
+            for (String item : values) {
+                if (item.charAt(item.length() - 1) != '.') {
+                    res.remove(item);
+                }
+                char[] chars = item.toCharArray();
+                if (!Character.isUpperCase(chars[0])) {
+                    res.remove(item);
+                }
+                int numberOfSpaces = 0;
+                for (int i = 0; i < chars.length; i++) {
+                    if (chars[i] == ' ') {
+                        numberOfSpaces++;
+                    }
+                }
+                if (numberOfSpaces < 3) {
+                    res.remove(item);
+                }
+            }
+            return res;
+        };
     }
 
     public Function<List<String>, Map<String, Integer>> stringSize() {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return x -> {
+            Map<String, Integer> result = new HashMap<>();
+            for (String item:x){
+                result.put(item, item.length());
+            }
+            return result;};
     }
 
     public BiFunction<List<Integer>, List<Integer>, List<Integer>> concatList() {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return (list1, list2) -> {
+            List<Integer> res = new ArrayList<>();
+            res.addAll(list1);
+            res.addAll(list2);
+            return res;
+        };
     }
 }
